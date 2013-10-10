780c579c3358d0c4ce4f127cf33229f9146f9138
Shindo N. Strzelczyk <shindo@Shindos-MacBook-Pro.local>
2013-10-10 11:25:22 -0700

    new rails project hooray

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kittenz::Application.config.secret_token = '52d96e36075cbb5379a6e7bcaf1eee3ebf0e4e5133afee92d7be26c7af1e6e2a9b617648caee8cea5d4d01c3a12f16ea65f1864cd642fcd4446889e159379eba'
